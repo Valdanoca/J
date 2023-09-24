63de1c990e10019de66aa7cb34cd4773299d689a
Valdanoca <valdanoca98@gmail.com>
2023-09-23 21:35:00 -0300

    minha penultima coomit

--- valdano/script.js
+++ valdano/script.js
@@ -1,27 +1,5 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-    const adicionarServicoBtn = document.getElementById("adicionar-servico-btn");
-    const listaServicos = document.getElementById("lista-servicos");
+const ad =()=>{
 
-    adicionarServicoBtn.addEventListener("click", function () {
-        const novoServico = prompt("Digite o nome do novo serviço:");
-        if (novoServico) {
-            const novoItem = document.createElement("li");
-            novoItem.textContent = novoServico;
-            listaServicos.appendChild(novoItem);
-        }
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    const listaServicos = document.getElementById("lista-servicos");
-
-    listaServicos.addEventListener("click", function (event) {
-        if (event.target.tagName === "LI") {
-            const confirmarExclusao = confirm("Deseja realmente excluir este serviço?");
-            if (confirmarExclusao) {
-                event.target.remove();
-            }
-        }
-    });
-});
+    alert('Configuração com sucesso');
+}
