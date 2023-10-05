f05179be7e0443e96120b327ac2235f9ecc93dfa
Valdanoca <valdanoca98@gmail.com>
2023-10-05 12:28:41 -0300

    1commit de java

--- valdano/script.js
+++ valdano/script.js
@@ -2,4 +2,38 @@
 const ad =()=>{
 
     alert('Configuração com sucesso');
-}
+    }
+
+   console.log(" Esta é a minha página oficial")
+   var cadi=90
+     console.log(cadi)
+   let bo = 'mama';
+     console.log(bo);
+     console.log(typeof cadi)
+
+  var objet = {
+      pai: "FAUSTINO  CÁ",
+      mae : "DOMINGAS CORREIA DJÚ",
+     irma : 'DEBURA GOMES',
+    }
+
+    console.log(objet);
+    console.log(typeof objet);
+    console.log(objet.irma);
+
+    var Debura = 14;
+    irma = 7
+    console.log(" O Nome dela é:" + irma);
+    console.log(typeof irma)
+
+     if (Debura>irma){
+       console.log("A Na verdade a é a mais velha e tem " + Debura + " Anos"); 
+      } 
+    else{
+     console.log("Carra a Debura afinal é a mais nova com " + Debura)
+       }
+
+  for( ; irma<=Debura; irma++){
+    console.log(irma) 
+     }
+ 
